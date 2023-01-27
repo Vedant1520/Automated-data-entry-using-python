5987cee5119dbf4f92592a11a27c55bf77cc0e0a
Vedant Kandalkar <99177441+Vedant1520@users.noreply.github.com>
2023-01-27 22:10:33 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,19 +3,19 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-i","--input_file",help="Ex :- input file = newCR.xlsx")
+parser.add_argument("-i","--input_file",help="Ex :- input file = ExcelFilename.xlsx")
 parser.add_argument("-o","--output_file",help="Ex :- output file =output/ ")
-parser.add_argument("-n","--name",help="Ex :- Prefix for filename = CAR")
+parser.add_argument("-n","--name",help="Ex :- Prefix for filename = webpagePrefix")
 
 args = parser.parse_args()
 
 if args.input_file and args.output_file and args.name:
-    xltohtml.input_file = args.input_file   #"newCR.xlsx"
-    xltohtml.output_file= args.output_file  #"output/"
-    xltohtml.name= args.name                #"CAR"               
+    xltohtml.input_file = args.input_file   
+    xltohtml.output_file= args.output_file  
+    xltohtml.name= args.name                              
     xltohtml.run()
     print("PAGES WERE CREATED!!")
     print("To check the pages open the given output folder.")
 else:
   print("You can use the below to learn the usage")
-  print("script.exe -i D:\car\input\Excel_filename.xlsx -o D:\car\output\ -n CAR")
+  print("script.exe -i User:\FolderName\inout\Excel_filename.xlsx -o User:\FloderName\output\ -n webpagePrefix")
